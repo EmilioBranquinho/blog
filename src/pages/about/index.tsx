326162dece0a671550905a456bdea98b7a9d6a4e
Emílio Marcelino Honório Branquinho <emiliomarcelino400@gmail.com>
2025-10-30 06:45:47 -0700

    creating post details pages

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,7 +1,31 @@
+import Head from "next/head";
+import style from './about.module.scss'
+import { FaFacebook } from "react-icons/fa";
+import { BsFacebook, BsInstagram, BsTwitter, BsWhatsapp } from "react-icons/bs";
 export default function About(){
     return(
         <>
-        <h1>Pagina about</h1>
+        <Head>
+            <title>Sobre o blog | 4EyesDuDe</title>
+        </Head>
+
+        <main className={style.container}>
+            <div className={style.containerHeader}>
+                <section className={style.ctaText}>
+                    <h1>4EyesDude</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus quia a unde similique praesentium voluptas, velit rem? Cupiditate dolore sed nobis, itaque pariatur, beatae minus perspiciatis nihil molestias doloremque dignissimos.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptate eligendi minus. Ipsa placeat vitae ratione earum accusamus mollitia blanditiis magnam aut? Quam mollitia minima suscipit ipsam eveniet corrupti ab?.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt magnam facere obcaecati odio magni quibusdam quos commodi minus optio? Sit quos suscipit perferendis enim distinctio! Nostrum deserunt necessitatibus corporis aspernatur.
+                    </p>
+                    <a href=""><BsFacebook/></a>
+                    <a href=""><BsInstagram/></a>
+                    <a href=""><BsWhatsapp/></a>
+                    <a href=""><BsTwitter/></a>
+                </section>
+
+                <img src="/assets/logo.png" alt="" />
+            </div>
+        </main>
         </>
     )
 }
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,14 +1,17 @@
 import { GetServerSideProps } from 'next'
 import style from './post.module.scss'
 import { getPrismicClient } from '@/services/prismic';
-import { asText } from '@prismicio/client';
+import { asHTML, asText } from '@prismicio/client';
+import Head from 'next/head';
+import Image from 'next/image';
 
 interface PostProps{
     id: string,
     uid: string,
     post_title: string,
     post_cover: string,
-    post_description: string    
+    post_description: string  ,
+    updatedAt: string;  
 }
 
 interface Post{
@@ -19,13 +22,30 @@ export default function Post({postData}: Post){
 
     return(
         <>
-        <h1>{postData.post_title}</h1>
-        <p>{[postData.post_description]}</p>
+        <Head>
+            <title>{postData.post_title}</title>
+        </Head>
+        <main className={style.container}>
+            <article className={style.post}>
+                <Image
+                src={postData.post_cover}
+                quality={100}
+                width={720}
+                height={410}
+                alt={postData.post_title}
+                placeholder='blur'
+                blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNksPn/AAADnAIdiEsNQAAAAABJRU5ErkJggg=='
+                />
+            <h1>{postData.post_title}</h1>
+            <time>{postData.updatedAt}</time>
+            <div className={style.postContent} dangerouslySetInnerHTML={{__html: postData.post_description}}></div>
+           </article>
+        </main>
         </>
     )
 }
 
-export const getServerSideProps: GetServerSideProps = async({ params, req }) =>{
+export const getServerSideProps: GetServerSideProps = async({ params, req }) => {
 
     const slug = params?.slug as string;
     
@@ -47,7 +67,7 @@ export const getServerSideProps: GetServerSideProps = async({ params, req }) =>{
         uid: response.uid,
         post_title: response.data.post_title,
         post_cover: response.data.post_cover.url,
-        post_description: asText(response.data.post_description),
+        post_description: asHTML(response.data.post_description),
         updatedAt: new Date(response.last_publication_date).toLocaleDateString("pt-Br", {
             day: "2-digit",
             month: "long",
